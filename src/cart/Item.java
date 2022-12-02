64f2ca67b0268ba2e47ceda25d203b57dbb0ae04
Tanna <tanna1987@gmail.com>
2022-12-02 17:06:16 +0800

    day02_workshop

---
+++ src/cart/Item.java
@@ -0,0 +1,46 @@
+package src.cart;
+
+public class Item {
+
+    // members
+    private String name = "";
+    private String code = "";
+    private Float price = 0f;
+    private int quantity = 0;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float totalPrice() {
+        return this.price * this.quantity;
+    }
+    
+    public Item(String code, String name) {
+        this.code = code;
+        this.name = name;
+
+    }
+
+}
